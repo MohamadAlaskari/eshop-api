7e1cf71d6e924dfc0c8879e507249f080262ddbe
MohamadAlaskari <m_alskari96@hotmail.com>
2025-03-04 00:52:53 +0100

    ohne glopal config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(typeOrmConfig), UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,12 +2,12 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: 'srv972.hstgr.io',
+  host: process.env.DB_HOST,
   port: 3306,
-  username: 'u252525807_alaskari',
-  password: '96E-Shop_db96+eshop_db.',
-  database: 'u252525807_eshop_db',
-  autoLoadEntities: true,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   synchronize: true,
+  autoLoadEntities: true,
   logging: true,
 };
