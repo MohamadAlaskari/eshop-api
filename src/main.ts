7c2ba16cd009aa0f56b99089fb6df0dfe01f4d7f
MohamadAlaskari <m_alskari96@hotmail.com>
2025-03-04 22:00:20 +0100

    swagger installation

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { GlobalExceptionFilter } from './common/filters/global-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new GlobalExceptionFilter());
   app.useGlobalPipes(new ValidationPipe());
+
+  //swagger config
+  const config = new DocumentBuilder()
+    .setTitle('E-Shop Backend')
+    .setDescription(
+      'A scalable and modular e-commerce backend built with NestJS, TypeORM, and MySQL.',
+    )
+    .setVersion('1.0')
+    //.addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  //http://localhost:3000/swagger/
+  SwaggerModule.setup('swagger', app, documentFactory);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
